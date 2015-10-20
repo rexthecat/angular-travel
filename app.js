e641d32e922bb6f7168a732f00b00e8104bc40b6
Anton <anton.steblin@gmail.com>
2015-10-21 00:52:48 +0300

    Add localStorage and fix code according to the tips

--- app.js
+++ app.js
@@ -5,35 +5,11 @@ app.controller('MainController', MainController);
 function MainController($scope) {
   $scope.showForm = false;
   $scope.newTour = {title: null, country: null, text: null, price: null};
-  $scope.tours = [
-    {
-      title: "Чикаго",
-      country: "США",
-      text: "Население Чикаго (по данным переписи 2010 года) \
-      составляет 2 695 000 человек (белое население — около 45 %). \
-      Агломерация Чикаго (с различными пригородами) называется \
-      «Большой Чикаго» или «Страна Чикаго» (англ. Chicagoland; \
-      название предложено газетой Chicago Tribune в начале XX века);\
-      в ней проживает около 9,5 млн человек. Агломерация Чикаго занимает \
-      26-е место в мире по числу жителей. \
-      Чикаго по праву считается экономической, промышленной, \
-      транспортной и культурной столицей Среднего Запада. Неофициально \
-      его иногда также называют «Второй Город» и «Город ветров». \
-      Впервые Чикаго был назван «Городом ветров» в статье в Chicago \
-      Tribune за 1858 год.",
-      price: 43490
-    },
-    {
-      title: "Рим",
-      country: "Италия",
-      text: "Рим — один из старейших городов мира, древняя столица \
-      Римской империи. Ещё в Античности (III век н. э.) Рим стали часто \
-      называть Вечным (лат. Roma Aeterna). Одним из первых так назвал Рим \
-      римский поэт Альбий Тибулл (I век до н. э.) в своей второй элегии. \
-      Представления о «вечности» Рима во многом сохранились и после падения \
-      древнеримской цивилизации, принеся соответствующий эпитет в современные языки.",
-      price: 127990
-    }];
+  $scope.tours = angular.fromJson(localStorage.getItem('thtour')) || [];
+
+  function updateLocalStorage() {
+    localStorage.setItem('thtour', angular.toJson($scope.tours));
+  }
 
   function clearTourObj(obj) {
     for (var key in obj) {
@@ -45,12 +21,7 @@ function MainController($scope) {
 
   $scope.editTour = function(tour) {
     tour.hide = true;
-    tour.currentTour = {
-      title: tour.title,
-      country: tour.country,
-      text: tour.text,
-      price: tour.price
-    };
+    tour.currentTour = angular.copy(tour);
   };
 
   $scope.cancelEdit = function(tour) {
@@ -63,20 +34,22 @@ function MainController($scope) {
 
   $scope.saveChanges = function(tour) {
     tour.hide = !tour.hide;
+    updateLocalStorage();
   };
 
   $scope.addTour = function() {
     $scope.newTour.price = $scope.newTour.price || 0;
-    $scope.tours.push(angular.copy($scope.newTour));
+    $scope.tours.unshift(angular.copy($scope.newTour));
     clearTourObj($scope.newTour);
+    updateLocalStorage();
   };
 
-  $scope.removeTour = function(tour) {
-    var idx = $scope.tours.indexOf(tour);
+  $scope.removeTour = function(tour, idx) {
     $scope.tours.splice(idx, 1);
+    updateLocalStorage();
   };
 
-  $scope.cancelSave = function() {
+  $scope.cancelCreate = function() {
     $scope.showForm = !$scope.showForm;
     clearTourObj($scope.newTour);
   };
